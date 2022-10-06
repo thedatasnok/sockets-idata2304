54570987fe881a59e3369167eba588c2cc42bbc4
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2022-10-06 13:47:43 +0200

    feat: implement udp client

--- udp-client/src/main/java/no/ntnu/iir/idata2304/udp/UdpClient.java
+++ udp-client/src/main/java/no/ntnu/iir/idata2304/udp/UdpClient.java
@@ -1,7 +1,10 @@
 package no.ntnu.iir.idata2304.udp;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 
 import no.ntnu.iir.idata2304.shared.Client;
 import no.ntnu.iir.idata2304.shared.ClientResponseHandler;
@@ -10,20 +13,43 @@ import no.ntnu.iir.idata2304.shared.ValidationUtils;
 public class UdpClient implements Client {
   private InetAddress address;
   private int port;
+  private DatagramSocket socket;
+  private byte[] responseBuffer;
+  private ClientResponseHandler responseHandler;
 
-  public UdpClient(InetAddress address, int port) {
+  public UdpClient(InetAddress address, int port) throws SocketException {
     this.address = address;
     this.port = ValidationUtils.validatePort(port);
+    this.socket = new DatagramSocket();
+    this.responseBuffer = new byte[1024];
   }
 
   @Override
   public void send(byte[] requestData) throws IOException {
-    // TODO: implement this method
+    DatagramPacket requestPacket = new DatagramPacket(
+      requestData, 
+      requestData.length, 
+      this.address, 
+      this.port
+    );
+
+    this.socket.send(requestPacket);
+
+    DatagramPacket responsePacket = new DatagramPacket(
+      this.responseBuffer,
+      this.responseBuffer.length
+    );
+
+    this.socket.receive(responsePacket);
+    if (this.responseHandler != null) {
+      byte[] newRequest = this.responseHandler.onResponse(this.responseBuffer, responsePacket.getLength());
+      if (newRequest.length > 0) this.send(newRequest);
+    }
   }
 
   @Override
   public void setResponseHandler(ClientResponseHandler handler) {
-    // TODO: implement this method
+    this.responseHandler = handler;
   }
 
 }
